945e3a2c03be08bbee64057082e0aa9daa0dc06f
Xandy <xandycc@gmail.com>
2024-05-31 19:47:06 +0300

    Drawer Implementation

--- lib/screens/main_map_screen.dart
+++ lib/screens/main_map_screen.dart
@@ -1,45 +1,45 @@
-import 'package:flutter/material.dart';  
-import 'package:google_maps_flutter/google_maps_flutter.dart';  
-  
-class MainMapPage extends StatefulWidget {  
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class MainMapPage extends StatefulWidget {
   const MainMapPage({super.key});
 
   @override
   State<MainMapPage> createState() => _MainMapPageState();
-}  
-  
-class _MainMapPageState extends State<MainMapPage> {  
-  late GoogleMapController mapController; 
-  static final LatLng _center = const LatLng(45.521563, -122.677433);  
-  final Set<Marker> _markers = {};  
-  LatLng _currentMapPosition = _center;  
+}
+
+class _MainMapPageState extends State<MainMapPage> {
+  late GoogleMapController mapController;
+  static final LatLng _center = const LatLng(45.521563, -122.677433);
+  final Set<Marker> _markers = {};
+  LatLng _currentMapPosition = _center;
   late BuildContext auxContext;
-  
-  void _onAddMarkerButtonPressed() {  
-    setState(() {  
-      _markers.add(Marker(  
-        markerId: MarkerId(_currentMapPosition.toString()),  
-        position: _currentMapPosition, 
-        infoWindow: InfoWindow(  
-          title: 'Nice Place',  
-          snippet: 'Welcome to Poland',  
-          onTap:  () {
-                  Scaffold.of(auxContext).openDrawer();
-                },
-        ),  
-        icon: BitmapDescriptor.defaultMarker,  
-      ));  
-    });  
-  }  
-  
-  void _onCameraMove(CameraPosition position) {  
-    _currentMapPosition = position.target;  
-  }  
-  
-  void _onMapCreated(GoogleMapController controller) {  
-    mapController = controller;  
-  }  
-  
+
+  void _onAddMarkerButtonPressed() {
+    setState(() {
+      _markers.add(Marker(
+        markerId: MarkerId(_currentMapPosition.toString()),
+        position: _currentMapPosition,
+        infoWindow: InfoWindow(
+          title: 'Interesting Event',
+          snippet: 'View more details here!',
+          onTap: () {
+            Scaffold.of(auxContext).openEndDrawer();
+          },
+        ),
+        icon: BitmapDescriptor.defaultMarker,
+      ));
+    });
+  }
+
+  void _onCameraMove(CameraPosition position) {
+    _currentMapPosition = position.target;
+  }
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
   @override
   void initState() {
     super.initState();
@@ -49,64 +49,135 @@ class _MainMapPageState extends State<MainMapPage> {
   void dispose() {
     super.dispose();
   }
-  @override  
-  Widget build(BuildContext context) {  
-    return MaterialApp(  
-      home: Scaffold(  
-        appBar: AppBar(  
-          title: Text('Flutter Maps Demo'),  
-          backgroundColor: Colors.green,  
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Marker Mapper',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          backgroundColor: Colors.black87,
           leading: Builder(
             builder: (context) {
               auxContext = context;
               return IconButton(
-                icon: const Icon(Icons.menu),
+                icon: const Icon(
+                  Icons.menu,
+                  color: Colors.white,
+                ),
                 onPressed: () {
                   Scaffold.of(context).openDrawer();
                 },
               );
             },
           ),
-        ),  
+        ),
         drawer: Drawer(
           child: ListView(
             padding: EdgeInsets.zero,
             children: [
-              const DrawerHeader(
-                decoration: BoxDecoration(
-                  color: Colors.blue,
+              const UserAccountsDrawerHeader(
+                currentAccountPicture: CircleAvatar(
+                    backgroundImage:
+                        AssetImage('assets/vectors/default_profile.png')),
+                accountEmail: Text('john.doe@example.com'),
+                accountName: Text(
+                  'John Doe',
+                  style: TextStyle(fontSize: 24.0),
                 ),
-                child: Text('Drawer Header'),
+                decoration: BoxDecoration(
+                  color: Colors.black87,
+                ),
+              ),
+              ListTile(
+                leading: const Icon(Icons.settings),
+                title: const Text(
+                  'Settings',
+                  style: TextStyle(fontSize: 24.0),
+                ),
+              ),
+              ListTile(
+                leading: const Icon(Icons.ads_click),
+                title: const Text(
+                  'Markers',
+                  style: TextStyle(fontSize: 24.0),
+                ),
+              ),
+              ListTile(
+                leading: const Icon(Icons.alarm),
+                title: const Text(
+                  'Upcoming',
+                  style: TextStyle(fontSize: 24.0),
+                ),
               ),
             ],
           ),
         ),
-        body: Stack(  
-          children: <Widget>[  
-            GoogleMap(  
-              onMapCreated: _onMapCreated,  
-              initialCameraPosition: CameraPosition(  
-                target: _center,  
-                zoom: 10.0,  
-              ),  
-              markers: _markers,  
-              onCameraMove: _onCameraMove  
-            ),  
-            Padding(  
-              padding: const EdgeInsets.all(14.0),  
-              child: Align(  
-                alignment: Alignment.topRight, 
-                child: FloatingActionButton(  
-                  onPressed: _onAddMarkerButtonPressed, 
-                  materialTapTargetSize: MaterialTapTargetSize.padded,  
-                  backgroundColor: Colors.green,  
-                  child: const Icon(Icons.map, size: 30.0),  
-                ),  
-              ),  
-            ),  
-          ],  
-        ),  
-      ),  
-    );  
-  }  
-}  
+        endDrawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              const UserAccountsDrawerHeader(
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage:
+                      AssetImage('assets/vectors/default_profile.png'),
+                ),
+                accountEmail: Text('best.post@example.com'),
+                accountName: Text(
+                  'Best Postington',
+                  style: TextStyle(fontSize: 24.0),
+                ),
+                decoration: BoxDecoration(
+                  color: Colors.black87,
+                ),
+              ),
+              ListTile(
+                leading: const Icon(Icons.house),
+                title: const Text(
+                  'Houses',
+                  style: TextStyle(fontSize: 24.0),
+                ),
+              ),
+              ListTile(
+                leading: const Icon(Icons.apartment),
+                title: const Text(
+                  'Apartments',
+                  style: TextStyle(fontSize: 24.0),
+                ),
+              ),
+            ],
+          ),
+        ),
+        body: Stack(
+          children: <Widget>[
+            GoogleMap(
+                onMapCreated: _onMapCreated,
+                initialCameraPosition: CameraPosition(
+                  target: _center,
+                  zoom: 10.0,
+                ),
+                markers: _markers,
+                onCameraMove: _onCameraMove),
+            Padding(
+              padding: const EdgeInsets.all(14.0),
+              child: Align(
+                alignment: Alignment.topRight,
+                child: FloatingActionButton(
+                  onPressed: _onAddMarkerButtonPressed,
+                  materialTapTargetSize: MaterialTapTargetSize.padded,
+                  backgroundColor: Colors.grey,
+                  child: const Icon(Icons.map, size: 30.0),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
