5ef418abd8660029515930585940a9aacc740f59
Xandy <xandycc@gmail.com>
2024-06-06 11:14:32 +0300

    Register and Login improvements

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -10,6 +10,8 @@ import '../values/app_routes.dart';
 import '../values/app_strings.dart';
 import '../values/app_theme.dart';
 
+import 'package:firebase_auth/firebase_auth.dart';
+
 class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
 
@@ -28,6 +30,30 @@ class _RegisterPageState extends State<RegisterPage> {
   final ValueNotifier<bool> passwordNotifier = ValueNotifier(true);
   final ValueNotifier<bool> confirmPasswordNotifier = ValueNotifier(true);
   final ValueNotifier<bool> fieldValidNotifier = ValueNotifier(false);
+
+  FirebaseAuth auth = FirebaseAuth.instance;
+  late User user;
+  Future<void> registerUserToFirebaseAndAddDataToFirestore() async {
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(
+              email: emailController.text, password: passwordController.text);
+      user = auth.currentUser;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        print('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        print('The account already exists for that email.');
+      }
+    } catch (e) {
+      print(e);
+    }
+
+    nameController.clear();
+    emailController.clear();
+    passwordController.clear();
+    confirmPasswordController.clear();
+  }
 
   void initializeControllers() {
     nameController = TextEditingController()..addListener(controllerListener);
@@ -225,10 +251,7 @@ class _RegisterPageState extends State<RegisterPage> {
                                 SnackbarHelper.showSnackBar(
                                   AppStrings.registrationComplete,
                                 );
-                                nameController.clear();
-                                emailController.clear();
-                                passwordController.clear();
-                                confirmPasswordController.clear();
+                                registerUserToFirebaseAndAddDataToFirestore();
                               }
                             : null,
                         child: const Text(AppStrings.register),
